b396e0ece07ca44ac7f243e699b3dac25db348f4
Clemens Hofreither <chofreither@gmail.com>
2016-02-29 04:26:13 +0100

    removed many now unneeded login screens

--- src/js/app.js
+++ src/js/app.js
@@ -41,46 +41,10 @@ app.config(function($routeProvider, $animateProvider) {
         templateUrl: 'tpl/login.html',
         controller: require('./controller/login/login')
     });
-    $routeProvider.when('/add-account', {
-        templateUrl: 'tpl/add-account.html',
-        controller: require('./controller/login/add-account')
-    });
-    $routeProvider.when('/create-account', {
-        templateUrl: 'tpl/create-account.html',
-        controller: require('./controller/login/create-account')
-    });
-    $routeProvider.when('/validate-phone', {
-        templateUrl: 'tpl/validate-phone.html',
-        controller: require('./controller/login/validate-phone')
-    });
     $routeProvider.when('/login-set-credentials', {
         templateUrl: 'tpl/login-set-credentials.html',
         controller: require('./controller/login/login-set-credentials')
     });
-/*    $routeProvider.when('/login-privatekey-upload', {
-        templateUrl: 'tpl/login-privatekey-upload.html',
-        controller: require('./controller/login/login-privatekey-upload')
-    });
-    $routeProvider.when('/login-verify-public-key', {
-        templateUrl: 'tpl/login-verify-public-key.html',
-        controller: require('./controller/login/login-verify-public-key')
-    });
-    $routeProvider.when('/login-existing', {
-        templateUrl: 'tpl/login-existing.html',
-        controller: require('./controller/login/login-existing')
-    });
-    $routeProvider.when('/login-initial', {
-        templateUrl: 'tpl/login-initial.html',
-        controller: require('./controller/login/login-initial')
-    });
-    $routeProvider.when('/login-new-device', {
-        templateUrl: 'tpl/login-new-device.html',
-        controller: require('./controller/login/login-new-device')
-    });
-    $routeProvider.when('/login-privatekey-download', {
-        templateUrl: 'tpl/login-privatekey-download.html',
-        controller: require('./controller/login/login-privatekey-download')
-    }); */
     $routeProvider.when('/account', {
         templateUrl: 'tpl/desktop.html',
         controller: require('./controller/app/navigation'),
