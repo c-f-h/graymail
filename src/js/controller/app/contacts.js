8ea9cff7d6fc30bb11175ac222516e5c38017924
Clemens Hofreither <chofreither@gmail.com>
2016-03-01 13:39:49 +0100

    stubbed out contacts page, maybe to be reused later

--- src/js/controller/app/contacts.js
+++ src/js/controller/app/contacts.js
@@ -4,7 +4,7 @@
 // Controller
 //
 
-var ContactsCtrl = function($scope, $q, keychain, pgp, dialog, appConfig) {
+var ContactsCtrl = function($scope) {
 
     //
     // scope state
@@ -18,43 +18,18 @@ var ContactsCtrl = function($scope, $q, keychain, pgp, dialog, appConfig) {
         }
     };
 
-    $scope.whiteoutKeyServer = appConfig.config.keyServerUrl.replace(/http[s]?:\/\//, ''); // display key server hostname
-
     //
     // scope functions
     //
 
     $scope.listKeys = function() {
-        return $q(function(resolve) {
-            resolve();
-
-        }).then(function() {
-            return keychain.listLocalPublicKeys();
-
-        }).then(function(keys) {
-            // add params to key objects
-            keys.forEach(function(key) {
-                var params = pgp.getKeyParams(key.publicKey);
-                _.extend(key, params);
-                key.fullUserId = key.userIds[0].name + ' <' + key.userIds[0].emailAddress + '>';
-            });
-            $scope.keys = keys;
-
-        }).catch(dialog.error);
+        // TODO fill $scope.keys
     };
 
-    $scope.getFingerprint = function(key) {
-        var fpr = key.fingerprint;
-        var formatted = fpr.slice(0, 4) + ' ' + fpr.slice(4, 8) + ' ' + fpr.slice(8, 12) + ' ' + fpr.slice(12, 16) + ' ' + fpr.slice(16, 20) + ' ... ' + fpr.slice(20, 24) + ' ' + fpr.slice(24, 28) + ' ' + fpr.slice(28, 32) + ' ' + fpr.slice(32, 36) + ' ' + fpr.slice(36);
-
-        $scope.fingerprint = formatted;
-    };
-
-    $scope.removeKey = function(key) {
-        return keychain.removeLocalPublicKey(key._id).then(function() {
-            // update displayed keys
-            return $scope.listKeys();
-        }).catch(dialog.error);
+    $scope.removeKey = function(/*key*/) {
+        // TODO
+        // update displayed keys
+        // return $scope.listKeys();
     };
 };
 
