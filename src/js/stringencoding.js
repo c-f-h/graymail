bb33aa851ba639ccdbb4927ec786fbbeeb3b9d6b
Clemens Hofreither <chofreither@gmail.com>
2016-03-10 18:14:12 +0100

    added comment to stringencoding.js

--- src/js/stringencoding.js
+++ src/js/stringencoding.js
@@ -1,5 +1,8 @@
 'use strict';
 
+// In the browser environment, we replace emailjs-stringencoding (which
+// is very large) with the native classes.
+
 module.exports = {
     TextEncoder: window.TextEncoder,
     TextDecoder: window.TextDecoder
